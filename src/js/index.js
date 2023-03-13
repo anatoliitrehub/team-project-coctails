45b014bd1bb19f64d139c1b90d7d2bf3841f7f91
adventuretimedi <olhadianaaa@gmail.com>
2023-03-13 16:18:16 +0200

    recommit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+'use strict';
+import { header } from './header';
+import { hero } from './hero';
+import { gallery } from './gallery'
+import { favoritecocktails } from './favoritecocktails';
+import { favoriteingredients } from './favoriteingredients';
+import { modalcocktails } from './modalcocktails';
+import { modalingredients } from './modalingredients';
+import { pagination } from './pagination'
+
+
+
+
