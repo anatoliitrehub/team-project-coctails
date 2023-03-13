32e414ef955d38003bc06f42cfb0f5503a2307dc
adventuretimedi <olhadianaaa@gmail.com>
2023-03-13 13:21:37 +0200

    adding mixins, container, editing html and js

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 'use strict';
 import { header } from './header';
 import { hero } from './hero';
-import { favorite } from './favorite';
-import { favoriteCocktails } from './favoriteCocktails';
-import { favoriteIngredients } from './favoriteIngredients';
-import { modalCocktails } from './modalCocktails';
-import { modalIngredients } from './modalIngredients';
+import { gallery } from './gallery'
+import { favoriteCocktails } from './favoritecocktails';
+import { favoriteIngredients } from './favoriteingredients';
+import { modalCocktails } from './modalcocktails';
+import { modalIngredients } from './modalingredients';
+import { pagination } from './pagination'
 
 
 
