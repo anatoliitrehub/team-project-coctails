b74ec13240f31225e0460e6c61fae1fb573ada9a
adventuretimedi <olhadianaaa@gmail.com>
2023-03-13 14:26:54 +0200

    fixes

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@
 import { header } from './header';
 import { hero } from './hero';
 import { gallery } from './gallery'
-import { favoriteCocktails } from './favoritecocktails';
-import { favoriteIngredients } from './favoriteingredients';
+import { favoritecocktails } from './favoritecocktails';
+import { favoriteingredients } from './favoriteingredients';
 import { modalcocktails } from './modalcocktails';
 import { modalingredients } from './modalingredients';
 import { pagination } from './pagination'
