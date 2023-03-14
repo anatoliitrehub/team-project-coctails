9bedf0a2a5ea33589528ebae58d6d1d84506421c
vya-vyu <linda1307@gmail.com>
2023-03-14 16:33:29 +0000

    mobile nav

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,27 @@
 "use strict"
-// import _header from "./sass/components/_header"
+
 
 const mobileMenuOpen = document.querySelector('.header__burger-menu');
-// const mobileMenuClose=
-const mobileMenu=document.querySelector('.header__burger-wrapper')
+const mobileMenuClose=document.querySelector('.header__mobile-menu-close')
+const mobileMenu=document.querySelector('.header__mobile-menu')
+const dropdownMenu = document.querySelector('.header__mobile-menu-dropdown-content')
+const dropdownBtn=document.querySelector('.mobile-menu-dropdown__btn')
 
-mobileMenuOpen.addEventListener('click', onToggle)
+
+
 
 function onToggle() { 
-    console.log('click');
+    
     mobileMenu.classList.toggle('header__mobile-menu-is-open');
 }
+
+function onDropdown(event) {
+    // console.dir(event.target.children[0].href.animVal)
+    dropdownMenu.classList.toggle('mobile-menu-dropdown-content-is-open')
+}
+// open dropdown
+
+// listeners
+mobileMenuOpen.addEventListener('click', onToggle)
+mobileMenuClose.addEventListener('click', onToggle)
+dropdownBtn.addEventListener('click', onDropdown)
