43741fec6f11b27959e1b282a8a30aa52bad5933
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-03-14 10:00:31 +0200

    delete something this <<<<<HEAD

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,7 @@ import { favoriteIngredients } from './favoriteingredients';
 import { modalCocktails } from './modalcocktails';
 import { modalIngredients } from './modalingredients';
 import { pagination } from './pagination'
-<<<<<<< HEAD
-=======
 
->>>>>>> 03bcf8b92d18a979f999daefcc697a5a7f5155a7
 
 
 
