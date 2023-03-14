9f1f883187096c4cf671bcce80b68c8449de1e62
Trehub Anatolii <tridfoto@gmail.com>
2023-03-14 22:08:58 +0200

    modify js

--- src/js/hero.js
+++ src/js/hero.js
@@ -2,7 +2,7 @@
 const hero = () =>{
     const tableTd = document.querySelector('.contain__table');
     const charsItems = [];
-    for (i=65; i<91; i++){
+    for (let i=65; i<91; i++){
     charsItems.push(String.fromCharCode(i))
 }
 
@@ -14,7 +14,7 @@ charsItems.push(String.fromCharCode(48))
 // console.log(charsItems)
 
 let sum = '';
-for (j=0;j<charsItems.length;j++){
+for (let j=0;j<charsItems.length;j++){
 if (j==0) sum +=`<tr>`;
 if (j==26) {
     sum += `<td class="table__item"></td>`;
