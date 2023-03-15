fd0d8f01eacae9f1513151e7965c247c2eb404f1
vya-vyu <linda1307@gmail.com>
2023-03-15 08:05:04 +0000

    init

--- src/js/header.js
+++ src/js/header.js
@@ -5,8 +5,10 @@ const mobileMenuOpen = document.querySelector('.header__burger-menu');
 const mobileMenuClose=document.querySelector('.header__mobile-menu-close')
 const mobileMenu=document.querySelector('.header__mobile-menu')
 const dropdownMenu = document.querySelector('.header__mobile-menu-dropdown-content')
-const dropdownBtn=document.querySelector('.mobile-menu-dropdown__btn')
-
+const dropdownBtn = document.querySelector('.mobile-menu-dropdown__btn')
+const htmlEL = document.documentElement;
+const themeBtn=document.querySelector('.header__switch [type="checkbox"]')
+console.log(themeBtn)
 
 
 
@@ -19,9 +21,19 @@ function onDropdown(event) {
     // console.dir(event.target.children[0].href.animVal)
     dropdownMenu.classList.toggle('mobile-menu-dropdown-content-is-open')
 }
-// open dropdown
+function onTheme(event) { 
+    console.log(event.target.checked)
+    if (event.target.checked) {
+        htmlEL.classList.remove('light')
+        htmlEL.classList.add('dark')
+    } else { 
+         htmlEL.classList.remove('dark')
+        htmlEL.classList.add('light')
+    }
+}
 
 // listeners
 mobileMenuOpen.addEventListener('click', onToggle)
 mobileMenuClose.addEventListener('click', onToggle)
 dropdownBtn.addEventListener('click', onDropdown)
+themeBtn.addEventListener('click',onTheme)
