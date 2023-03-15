be580f3651675dd5e66954100b466c7092364e5e
vya-vyu <linda1307@gmail.com>
2023-03-15 15:28:45 +0000

    done theme and add theme to localStorage

--- src/js/header.js
+++ src/js/header.js
@@ -10,7 +10,14 @@ const htmlEL = document.documentElement;
 const themeBtnHeader = document.querySelector('.header__switch [type="checkbox"]')
 const currentThemeLightHeader = document.querySelector('.theme__list-light');
 const currentThemeDarkHeader = document.querySelector('.theme__list-dark');
+const mobileMenuThemeBtn = document.querySelector('.header__switch-mobile-menu [type="checkbox"]');
+const currentThemeLightMobileMenu = document.querySelector('.theme__list--mobile-menu-ligth');
+const currentThemeDarkMobileMenu = document.querySelector('.theme__list--mobile-menu-dark');
 
+
+if (localStorage.getItem('theme')) {
+    onThemeSet(localStorage.getItem('theme'))
+} 
 
 
 
@@ -24,25 +31,49 @@ function onDropdown(event) {
     dropdownMenu.classList.toggle('mobile-menu-dropdown-content-is-open')
 }
 
-function onTheme(event) { 
-    console.log(event.target.checked)
-    if (event.target.checked) {
+function setTheme(themeName) { 
+    localStorage.setItem('theme', themeName);
+}
+
+function onThemeSet(themeName) { 
+    
+    if (themeName==='dark') {
         htmlEL.classList.remove('light')
         htmlEL.classList.add('dark')
         currentThemeLightHeader.classList.remove('header__current');
         currentThemeDarkHeader.classList.add('header__current');
-
+        currentThemeLightMobileMenu.classList.remove('header__current');
+        currentThemeDarkMobileMenu.classList.add('header__current');
+        setTheme('dark');
+        themeBtnHeader.checked = true;
+        mobileMenuThemeBtn.checked = true;
+        
     } else { 
          htmlEL.classList.remove('dark')
         htmlEL.classList.add('light')
         currentThemeDarkHeader.classList.remove('header__current');
-         currentThemeLightHeader.classList.add('header__current');
+        currentThemeLightHeader.classList.add('header__current');
+        currentThemeDarkMobileMenu.classList.remove('header__current');
+        currentThemeLightMobileMenu.classList.add('header__current');
+        setTheme('light');
+        themeBtnHeader.checked = false;
+        mobileMenuThemeBtn.checked = false;
         
     }
 }
+
+function onTheme(event) { 
+    if (event.target.checked) { 
+        onThemeSet('dark')
+    } else {
+        onThemeSet('light')
+    }
+}
+
 
 // listeners
 mobileMenuOpen.addEventListener('click', onToggle)
 mobileMenuClose.addEventListener('click', onToggle)
 dropdownBtn.addEventListener('click', onDropdown)
-themeBtnHeader.addEventListener('click',onTheme)
+themeBtnHeader.addEventListener('click', onTheme)
+mobileMenuThemeBtn.addEventListener('click',onTheme)
