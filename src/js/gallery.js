9c7563d547cfdd3ed9d8b312feeca3cf491cde7c
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-03-15 18:58:34 +0200

    commit befor pull

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -6,6 +6,7 @@ import { FetchCocktails } from './fetch';
 
 const galleryListEl = document.querySelector('.gallery__list');
 
+
 const fetchCocktails = new FetchCocktails();
 const arr = [];
 
@@ -23,7 +24,22 @@ for (let index = 0; index < 9; index++) {
 
 Promise.all(arr).then(result => {
   result.forEach(res => {
-    const drink = [res.data.drinks[0]];
+    const drink = [res.data.drinks[0]]; 
     galleryListEl.insertAdjacentHTML('beforeend', galleryMarkUp(drink));
   })
 });
+
+
+// const handleToggleBtn = ({ target }) => {
+//   // console.log(target.textContent);
+//   if (target.classList.contains('gallery__figcaption--storage')) {
+//     target.textContent = `Remove`;
+//     return;
+//   }
+//   if (target.textContent === 'Remove') {
+//     target.textContent = 'Add to'
+//   }
+// }
+
+
+// galleryListEl.addEventListener('click', handleToggleBtn)
