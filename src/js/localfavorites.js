697eba42eaf748e21743e121135ec628838ae6e7
Trehub Anatolii <tridfoto@gmail.com>
2023-03-17 00:59:20 +0200

    update removeLock

--- src/js/localfavorites.js
+++ src/js/localfavorites.js
@@ -15,8 +15,8 @@ const localFavorites = {
     removeLocal(key,obj){
         let loc = (localStorage.getItem(key)) ? JSON.parse(localStorage.getItem(key)) : [];
         let temp = [];
-        if(key='favcock') temp = loc.filter(item => item.idDrink!==obj.idDrink);
-        if(key='favingr') temp = loc.filter(item => item.idIngredient!==obj.idIngredient);
+        if(key=='favcockt') temp = loc.filter(item => item.idDrink!==obj.idDrink);
+        if(key=='favingr') temp = loc.filter(item => item.idIngredient!==obj.idIngredient);
         console.log('temp',temp)
         localStorage.setItem(key,JSON.stringify(temp));
     }
