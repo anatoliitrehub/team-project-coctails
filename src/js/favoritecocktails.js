5373fe1d3c656e210bc68845f0f7d4dca59d2bae
adventuretimedi <olhadianaaa@gmail.com>
2023-03-18 01:07:58 +0200

    add favorite ingredients

--- src/js/favoritecocktails.js
+++ src/js/favoritecocktails.js
@@ -1,1 +1,6 @@
-// comment
+"use strict";
+import { localFavorites } from './localfavorites';
+import { header } from './header';
+import { modalcocktails } from './modalcocktails';
+import { modalingredients } from './modalingredients';
+import { renderPagination } from './pagination-show'
--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,6 @@ import { localFavorites } from './localfavorites';
 import { header } from './header';
 import { hero } from './hero';
 import { gallery } from './gallery'
-import { favoritecocktails } from './favoritecocktails';
-import { favoriteingredients } from './favoriteingredients';
 import { modalcocktails } from './modalcocktails';
 import { modalingredients } from './modalingredients';
 import { renderPagination } from './pagination-show'
