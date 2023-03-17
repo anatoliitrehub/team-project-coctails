c5dc5dc7f0bdf698f679cf52333b48a87487adef
Trehub Anatolii <tridfoto@gmail.com>
2023-03-17 10:58:12 +0200

    update remove local

--- src/js/localfavorites.js
+++ src/js/localfavorites.js
@@ -1,4 +1,5 @@
 'use strict';
+import Notiflix from 'notiflix';
 
 const localFavorites = {
 
@@ -10,15 +11,49 @@ const localFavorites = {
         let loc = (localStorage.getItem(key)) ? JSON.parse(localStorage.getItem(key)) : [];
         loc.push(obj);
         localStorage.setItem(key,JSON.stringify(loc));
+        Notiflix.Notify.success('Item add to favorites successfully');
+        return true;
     },
 
     removeLocal(key,obj){
-        let loc = (localStorage.getItem(key)) ? JSON.parse(localStorage.getItem(key)) : [];
-        let temp = [];
-        if(key=='favcockt') temp = loc.filter(item => item.idDrink!==obj.idDrink);
-        if(key=='favingr') temp = loc.filter(item => item.idIngredient!==obj.idIngredient);
-        console.log('temp',temp)
-        localStorage.setItem(key,JSON.stringify(temp));
+        let lococ = (localStorage.getItem("favcock")) ? JSON.parse(localStorage.getItem("favcock")) : [];
+        let tempCoc = [];
+        let locin = (localStorage.getItem("favingr")) ? JSON.parse(localStorage.getItem("favingr")) : [];
+        let tempIng = [];
+
+        switch (key){
+            case "favcock":
+                // lococ = (localStorage.getItem("favcock")) ? JSON.parse(localStorage.getItem("favcock")) : [];
+                // tempCoc = [];
+                tempCoc = lococ.filter(item => item.idDrink!==obj.idDrink);
+                localStorage.setItem("favcock",[]);
+                localStorage.setItem("favcock",JSON.stringify(tempCoc));
+                Notiflix.Notify.success('Item remove from favorites successfully');
+                console.log("remove coc")
+            break;
+
+            case "favingr":
+                // locin = (localStorage.getItem("favingr")) ? JSON.parse(localStorage.getItem("favingr")) : [];
+                tempIng = locin.filter(item => item.idIngredient!==obj.idIngredient);
+                localStorage.setItem("favingr",[]);
+                localStorage.setItem("favingr",JSON.stringify(tempIng));
+                Notiflix.Notify.success('Item remove from favorites successfully');
+                console.log("remove ing")
+
+            break;
+
+            default:
+                Notiflix.Notify.info('Nothing to remove');
+ 
+        }
+        // let loc = (localStorage.getItem(key)) ? JSON.parse(localStorage.getItem(key)) : [];
+        // let temp = [];
+        // if(key=='favcockt') temp = loc.filter(item => item.idDrink!==obj.idDrink);
+        // if(key=='favingr') temp = loc.filter(item => item.idIngredient!==obj.idIngredient);
+        // console.log('temp',temp)
+        // localStorage.setItem(key,JSON.stringify(temp));
+        // Notiflix.Notify.success('Item remove from favorites successfully');
+        // return true;
     }
 }
 
