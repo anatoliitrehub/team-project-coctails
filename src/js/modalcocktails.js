b3a0cae9cf9d73ff4131e8c5ccb6c28a89c47c82
Dubyshkina <ek.kalyazina@gmail.com>
2023-03-18 19:42:03 +0200

    fixed all bugs with modal and buttons

--- src/js/modalcocktails.js
+++ src/js/modalcocktails.js
@@ -3,48 +3,85 @@ import { FetchCocktails } from './fetch';
 import { localFavorites } from './localfavorites';
 import { showIngredientDetails } from './modalingredients';
 
-const fetchCocktails = new FetchCocktails();
-const FAV_COCKT_KEY = "favcockt";
+
 const elemRefs = {
   openLearnMoreBtn: document.querySelector('.js-learn-more'),
   closeModalCockBtn: document.querySelector('[data-modal-close-cocktails]'),
   addBtn: document.querySelector('.js-add-btn'),
-  removeBtn: document.querySelector('.js-remove-btn'),
   backdrop: document.querySelector('[data-modal-backdrop]'),
-  body: document.querySelector('body'),
   title: document.querySelector('.cocktail__title'),
   cocktailImg: document.querySelector('.cocktail__photo'),
   ingridList: document.querySelector('.ingredients-list'),
   instruction: document.querySelector('.instraction__descr'),
-  // addBtn: document.querySelector('.js-add-btn'),
 };
 
+const fetchCocktails = new FetchCocktails();
+const FAV_COCKT_KEY = "favcockt";
+let cocktailDetail = {};
+
+// лисенер на модалку 
 elemRefs.closeModalCockBtn.addEventListener('click', toggleModalWindow);
 elemRefs.backdrop.addEventListener('click', onBackdropClick);
 
-function onBackdropClick(event) {
+// лисенер
+elemRefs.addBtn.addEventListener('click', event => {
+  if (elemRefs.addBtn.textContent === 'Add to favorites'){
+    localFavorites.addLocal(FAV_COCKT_KEY, cocktailDetail);
+    elemRefs.addBtn.textContent ='Remove from favorites';
+    let cocktailIdBtn = `[name='${cocktailDetail.idDrink}']`
+    let cardBtn = document.querySelector(cocktailIdBtn);
+    cardBtn.firstElementChild.textContent = 'Remove';
+    cardBtn.lastElementChild.style.fill = '#fd5103';
+    return;
+  }else {
+    localFavorites.removeLocal(FAV_COCKT_KEY, cocktailDetail);
+    elemRefs.addBtn.textContent ='Add to favorites';
+    let cocktailIdBtn = `[name='${cocktailDetail.idDrink}']`
+    let cardBtn = document.querySelector(cocktailIdBtn);
+    cardBtn.firstElementChild.textContent = 'Add to';
+    cardBtn.lastElementChild.style.fill = 'var(--white-modal-text)';
+  }
+});
+// лісенер на інгрідієнтах 
+elemRefs.ingridList.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'A' && event.target.nodeName !== 'SPAN') {
+    return;
+  }
+
+  const ingridName = event.target.lastChild.textContent
+    .toLowerCase()
+    .replace(' ', '%20');
+
+  fetchCocktails.fetchIngridientsByName(ingridName).then(res => {
+  
+    showIngredientDetails(res.data.ingredients[0]);
+  });
+});
+
+
+export function toggleModalWindow() {
+  elemRefs.backdrop.classList.toggle('is-hidden');
+
+  if (!elemRefs.backdrop.classList.contains('is-hidden')) {
+    document.body.style.overflowY = 'hidden';
+    
+  } else {
+    document.body.style.overflowY = 'auto';
+  }
+}
+export function onBackdropClick(event) {
   if (event.currentTarget === event.target) {
     toggleModalWindow();
   }
 }
-let cocktailId = {};
+
 export function showCocktailDetails(cocktail) {
-  cocktailId = {...cocktail};
+  cocktailDetail = {...cocktail};
   elemRefs.title.textContent = cocktail.strDrink;
   elemRefs.cocktailImg.src = cocktail.strDrinkThumb;
-
-  const dataCocktail = localFavorites
-    .getLocal(FAV_COCKT_KEY)
-    .find(obj => obj.idDrink === cocktail.idDrink);
-
-  if (dataCocktail) {
-    elemRefs.addBtn.classList.add('is-hidden-btn');
-    elemRefs.removeBtn.classList.remove('is-hidden-btn');
-  } else {
-    elemRefs.addBtn.classList.remove('is-hidden-btn');
-    elemRefs.removeBtn.classList.add('is-hidden-btn');
-  }
-
+  
   const ingridients = [];
   const measure = [];
 
@@ -71,51 +108,15 @@ export function showCocktailDetails(cocktail) {
   elemRefs.ingridList.insertAdjacentHTML('beforeend', str);
   elemRefs.instruction.textContent = cocktail.strInstructions;
 
+  const dataCocktail = localFavorites
+    .getLocal(FAV_COCKT_KEY)
+    .find(obj => obj.idDrink === cocktail.idDrink);
+
+  if (dataCocktail) {
+    elemRefs.addBtn.textContent ='Remove from favorites';
+  } else {
+    elemRefs.addBtn.textContent ='Add to favorites';
+  }
+
   toggleModalWindow();
 }
-
-
-elemRefs.addBtn.addEventListener('click', event => {
-  localFavorites.addLocal(FAV_COCKT_KEY, cocktailId);
-  elemRefs.addBtn.classList.add('is-hidden-btn');
-  elemRefs.removeBtn.classList.remove('is-hidden-btn');
-});
-
-elemRefs.removeBtn.addEventListener('click', event => {
-  localFavorites.removeLocal(FAV_COCKT_KEY, cocktailId);
-  elemRefs.removeBtn.classList.add('is-hidden-btn');
-  elemRefs.addBtn.classList.remove('is-hidden-btn');
-});
- 
-
-
-elemRefs.ingridList.addEventListener('click', event => {
-  event.preventDefault();
-
-  if (event.target.nodeName !== 'A' && event.target.nodeName !== 'SPAN') {
-    return;
-  }
-
-  const ingridName = event.target.lastChild.textContent
-    .toLowerCase()
-    .replace(' ', '%20');
-
-  fetchCocktails.fetchIngridientsByName(ingridName).then(res => {
-    console.log(res);
-    showIngredientDetails(res.data.ingredients[0]);
-  });
-});
-
-export function toggleModalWindow() {
-  elemRefs.backdrop.classList.toggle('is-hidden');
-
-  if (!elemRefs.backdrop.classList.contains('is-hidden')) {
-    elemRefs.body.style.overflowY = 'hidden';
-  } else {
-    elemRefs.body.style.overflowY = 'auto';
-  }
-}
-// fetchCocktails.fetchIngridientsByName(ingridName).then(res => {
-//   console.log(res);
-
-// });
